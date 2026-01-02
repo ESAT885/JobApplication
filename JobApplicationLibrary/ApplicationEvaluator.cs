afd9b3117980eebcc40328ba18188c30e5c9d5c6
Muhammet Esat Demir <muhammetesatdemir@outlook.com>
2026-01-02 23:23:46 +0300

    Add project files.

---
+++ JobApplicationLibrary/ApplicationEvaluator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobApplicationLibrary
+{
+    public class ApplicationEvaluator
+    {
+        public ApplicationResult EvaluateApplication(Models.JobApplication form)
+        {
+            return ApplicationResult.AutoAccepted;
+        }
+    }
+    public enum ApplicationResult
+    {
+        AutoRejected,
+        TransferredToHR,
+        TransferredToLead,
+        TransferredToCTO,
+        AutoAccepted
+    }
+}
